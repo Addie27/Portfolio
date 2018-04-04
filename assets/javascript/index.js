1363c99618319757df60586d0dc5bebed791bb1d
Adeline Johnson <groblewski.adeline@gmail.com>
2018-04-03 20:51:27 -0400

    updates to main page

---
+++ assets/javascript/index.js
@@ -0,0 +1,16 @@
+
+var i = 0;
+var txt = '          I want to create clean and user-friendly applications.             Let me build one for you.'; /* The text */
+var speed = 60; /* The speed/duration of the effect in milliseconds */
+
+$(document).ready(function() {
+window.onload = function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("biobuild").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+    $('#mybutton').hide().delay(5000).fadeIn(2200);
+});
