5c448eca06b47bc00ff0d5d93bd7c944139af03a
Adeline Johnson <groblewski.adeline@gmail.com>
2018-04-04 17:20:29 -0400

    updated about and portfolio pages

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,20 @@
+var slide1 = function () {
+    $(".drop1").slideToggle("slow");
+};
+var slide2 = function () {
+    $(".drop2").slideToggle("slow");
+};
+var slide3 = function () {
+    $(".drop3").slideToggle("slow");
+};
+var slide4 = function () {
+    $(".drop4").slideToggle("slow");
+};
+
+$(document).ready(function () {
+    $("#flip1").on("click", slide1);
+    $("#flip2").on("click", slide2);
+    $("#flip3").on("click", slide3);
+    $("#flip3").on("click", slide4);
+
+});
